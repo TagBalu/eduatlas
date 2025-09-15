684b30aa6b5414f4e0966e7dc43c103d56bed488
TagBalu <baluyevslava@icloud.com>
2025-09-15 09:28:07 +0200

    base

---
+++ src/main/java/vacislavbaluyev/eduatlas/EduatlasApplication.java
@@ -0,0 +1,13 @@
+package vacislavbaluyev.eduatlas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduatlasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduatlasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vacislavbaluyev/eduatlas/EduatlasApplicationTests.java
@@ -0,0 +1,13 @@
+package vacislavbaluyev.eduatlas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduatlasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
